ba782e34904dcff592573df153598ed0787b9e7e
surtant236 <57491049+surtant236@users.noreply.github.com>
2025-12-15 22:26:55 +0700

    Update tru nguyen lieu trong kho

--- routes/recipes.js
+++ routes/recipes.js
@@ -12,6 +12,8 @@ router.get('/:id/total-time', recipeController.calculateTotalTime);
 
 router.get('/:id/check-ingredients', recipeController.checkIngredientsAvailability);
 
+router.post('/consume', recipeController.consumeRecipe);
+
 router.post('/', recipeController.createRecipe);
 
 router.put('/:id', recipeController.updateRecipe);
